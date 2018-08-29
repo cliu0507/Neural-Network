0f925531eeb900bfe27bfad334edf4ee9f148f16
cliu <cliu@ucdavis.edu>
2018-08-29 15:57:31 -0700

    new implementation in pascal_voc_parser to support train/test split

--- pascal_voc_util/pascal_voc_parser.py
+++ pascal_voc_util/pascal_voc_parser.py
@@ -7,7 +7,7 @@ import numpy as np
 import uuid
 
 
-def get_data(input_path):
+def get_data(input_path,train_test_split = False):
 	'''function to read the path to folder storing xml files and get data
 	args:
 		input_path: string
@@ -54,8 +54,26 @@ def get_data(input_path):
 				x2 = int(round(float(obj_bbox.find('xmax').text)))
 				y2 = int(round(float(obj_bbox.find('ymax').text)))
 				difficulty = int(element_obj.find('difficult').text) == 1
+				
+
+				#Init if_train variable to mark if this bbox is a training or test
+				if_train = 1
+
+				#if we enable train_test_split
+				if train_test_split:
+					train_proportion = train_test_split
+					test_proportion = 1 - train_proportion
+				
+					#random choose to mark it as training data or test data
+					if_train = np.random.choice(np.arange(0, 2), p=[0.34,0.66])
 				annotation_data['bboxes'].append(
-						{'class': class_name, 'x1': x1, 'x2': x2, 'y1': y1, 'y2': y2, 'difficult': difficulty})       
+						{	'class': class_name, 
+							'x1': x1, 
+							'x2': x2, 
+							'y1': y1, 
+							'y2': y2, 
+							'difficult': difficulty,
+							'if_train':if_train})       
 			all_imgs.append(annotation_data)
 			if visualise:
 				img = cv2.imread(annotation_data['filepath'])
@@ -70,29 +88,68 @@ def get_data(input_path):
 	return all_imgs, classes_count, class_mapping
 
 
-def cut_bndbox_to_img(input_path, output_data_path):
+def cut_bndbox_to_img(input_path, output_data_path,train_test_split=False):
 	#Parse XML file folder
-	all_imgs, classes_count, class_mapping=get_data(input_path)
+	all_imgs, classes_count, class_mapping=get_data(input_path,train_test_split=train_test_split)
+
 	if not os.path.exists(output_data_path):
 		os.makedirs(output_data_path)
+
+	output_data_path_train = os.path.join(output_data_path,"train")
+	
+	if not os.path.exists(output_data_path_train):
+		os.makedirs(output_data_path_train)
 	for class_name in classes_count.keys():
-		if not os.path.exists(os.path.join(output_data_path,str(class_name))):
-			os.makedirs(os.path.join(output_data_path,class_name))
+		if not os.path.exists(os.path.join(output_data_path_train,str(class_name))):
+			os.makedirs(os.path.join(output_data_path_train,class_name))
+	
+	# if enabling train test split, will store all output files to ./train and ./test folder
+	# otherwise, all files will be written to ./train
+	if train_test_split:
+		output_data_path_test  = os.path.join(output_data_path,"test")
+		if not os.path.exists(output_data_path_test):
+			os.makedirs(output_data_path_test)
+
+		for class_name in classes_count.keys():
+			if not os.path.exists(os.path.join(output_data_path_test,str(class_name))):
+				os.makedirs(os.path.join(output_data_path_test,class_name))
+
+
 	for annotation_data in all_imgs:
 		try:
 			filepath = annotation_data['filepath']
 			filename = annotation_data['filename']
 			width = annotation_data['width']
 			height = annotation_data['height']
-			for bbox in annotation_data['bboxes']:
-				png_filepath = os.path.join(output_data_path,bbox['class'],str(bbox['class'])+'_'+str(uuid.uuid4())+'_'+os.path.splitext(filename)[0]+'.png')
-				x1 = bbox['x1']
-				x2 = bbox['x2']
-				y1 = bbox['y1']
-				y2 = bbox['y2']
-				img = cv2.imread(filepath)
-				roi = img[y1:y2,x1:x2]
-				cv2.imwrite(png_filepath,roi)
+			for bbox in annotation_data['bboxes']:		
+				
+				#Check if train_test_split enabled
+				if train_test_split:
+					#Write all files to ./train and ./test folder
+					if_train = bbox['if_train']
+					x1 = bbox['x1']
+					x2 = bbox['x2']
+					y1 = bbox['y1']
+					y2 = bbox['y2']
+					if if_train == 1:
+						png_filepath = os.path.join(output_data_path_train,bbox['class'],str(bbox['class'])+'_'+str(uuid.uuid4())+'_'+os.path.splitext(filename)[0]+'.png')
+					elif if_train == 0:
+						png_filepath = os.path.join(output_data_path_test,bbox['class'],str(bbox['class'])+'_'+str(uuid.uuid4())+'_'+os.path.splitext(filename)[0]+'.png')
+					else:
+						raise ValueError("Wrong attribute 'if_train' value!")
+					img = cv2.imread(filepath)
+					roi = img[y1:y2,x1:x2]
+					cv2.imwrite(png_filepath,roi)
+				else:	
+					#Write all files to ./train folder
+					x1 = bbox['x1']
+					x2 = bbox['x2']
+					y1 = bbox['y1']
+					y2 = bbox['y2']
+					png_filepath = os.path.join(output_data_path_train,bbox['class'],str(bbox['class'])+'_'+str(uuid.uuid4())+'_'+os.path.splitext(filename)[0]+'.png')
+					img = cv2.imread(filepath)
+					roi = img[y1:y2,x1:x2]
+					cv2.imwrite(png_filepath,roi)
 		except Exception as e:
 			print(e)
 			continue
@@ -101,7 +158,7 @@ def cut_bndbox_to_img(input_path, output_data_path):
 
 #This is the test main function
 #all_imgs, classes_count, class_mapping= get_data("/Users/cliu/Data")
-cut_bndbox_to_img(input_path="/Users/cliu/Data",output_data_path="/Users/cliu/Documents/Github/nn/pascal_voc_util/data")
+cut_bndbox_to_img(input_path="/Users/cliu/Data",output_data_path="/Users/cliu/Documents/Github/nn/pascal_voc_util/data",train_test_split=True)
 
 
 
